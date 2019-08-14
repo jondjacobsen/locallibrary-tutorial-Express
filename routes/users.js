808098ca72511d6072a7c3c12174daf9971e27a0
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-08-13 21:30:41 -0400

    ReadMe Update - 8/13/2019

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* GET user detail. */
+router.get('./cool', function(req, res, next) {
+    res.send('This is the detail page for the user');
+});
+
 module.exports = router;
