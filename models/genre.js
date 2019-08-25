67c2958750731809191799a24e750306cfd86189
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-08-25 17:06:34 -0400

    Models Added, New Data added also - PopulateDb.js

---
+++ models/genre.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+    {
+        name: {type: String, required: true}
+    }
+);
+
+
+//Virtual for genre's URL
+GenreSchema
+    .virtual('url')
+    .get(function () {
+        return '/catalog/genre/'+ this_id;
+    });
+
+//Export Genre Model
+module.exports = mongoose.model('Genre', GenreSchema);
+
