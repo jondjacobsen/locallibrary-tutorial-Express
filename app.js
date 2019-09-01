3c2bfe16c9cd0d0e5cb1403bc3cb8d441d9e6307
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-09-01 09:37:18 -0400

    Added Wiki.js routing App.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coolRouter = require('./routes/cool');
-
+var wiki = require('./wiki.js');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/wiki', wiki);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
